d86c434a85241117c3b5f33e382eec22a2c6381b
Snehal <snehalbhole4@gmail.com>
2021-05-19 15:07:04 +0530

    Initial commit

---
+++ htmlpractice/new.js
@@ -0,0 +1,25 @@
+/*fs=document.createElement('p')
+fs.innerText='this is new para'
+
+console.log("hey you")
+console.error("wait an watch")*/
+/*alert("get out")*/
+
+
+
+
+/*EVENT HANDLING IN JS
+1.ONCLICK*/
+function clicks(){
+	console.log("the button is clicked")
+}
+
+function clicked(){
+	console.log("the button is clicked")
+}
+
+/*2.ONLOAD*/
+window.onload=function(){
+	console.log("onloaded successfully")
+
+}
